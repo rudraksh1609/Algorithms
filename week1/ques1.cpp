d5db66d528c36aaefb7fa33dec2e19c70969d1f5
rudraksh1609 <142419212+rudraksh1609@users.noreply.github.com>
2024-01-31 11:22:57 +0530

    Update ques1.cpp

--- week1/ques1.cpp
+++ week1/ques1.cpp
@@ -2,11 +2,31 @@
 using namespace std;
 int main()
 {
-  int arr[25],n,t;
+  int arr[25],n,t,y;
   cout<<"Enter the number of test cases"<<endl;
   cin>>t;
   for(int i=0; i<t; i++)
   {
-    cout<<"Enter size of array for test case"<<i+1<<endl;
+    cout<<"Enter size of array for test case"<< i+1 << endl;
+    cin>>n;
+    for(int j=0; j<n; j++)
+    {
+      cout<<"Enter the elements for array of size:"<< n << endl;
+      cin>>arr[j];
+    }
+    cout<<"Enter the elements to be searched:"<<endl;
+    cin>>y;
+    for(int j=0; j<n; j++)
+    {
+      if(arr[j]==y)
+      {
+        cout<<"Element Present at:"<< j+1 << endl;
+        break;
+      }
+      else
+      {
+        cout<<"Element not present"<<endl;
+      }
+    }
   }
 }
