f0b47bc3c75e162dc8f71285c13bf511740418d9
rudraksh1609 <142419212+rudraksh1609@users.noreply.github.com>
2024-01-31 11:07:11 +0530

    Update ques1.cpp

--- week1/ques1.cpp
+++ week1/ques1.cpp
@@ -1,1 +1,12 @@
-#include<s
+#include<iostream>
+using namespace std;
+int main()
+{
+  int arr[25],n,t;
+  cout<<"Enter the number of test cases"<<endl;
+  cin>>t;
+  for(int i=0; i<t; i++)
+  {
+    cout<<"Enter size of array for test case"<<i+1<<endl;
+  }
+}
