8cc0eff034ad41b3d2dee5a6eef032c9a32b1b68
rudraksh1609 <142419212+rudraksh1609@users.noreply.github.com>
2024-02-08 09:14:21 +0530

    Update Ques2.cpp

--- Week2/Ques2.cpp
+++ Week2/Ques2.cpp
@@ -1,1 +1,48 @@
-
+#include <iostream>
+using namespace std;
+bool findSequence(int arr[], int n)
+{
+    for (int k = n - 1; k >= 2; k--)
+    {
+        int i = 0, j = k - 1;
+        while (i < j)
+        {
+            int sum = arr[i] + arr[j];
+            if (sum == arr[k])
+            {
+                cout << i+1 << " " << j+1 << " " << k+1 << endl;
+                return true;
+            }
+            else if (sum < arr[k])
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+    }
+    cout << "No sequence found" << endl;
+    return false;
+}
+int main()
+{
+    int T;
+    cout << "Enter the number of test cases: ";
+    cin >> T;
+    for(int i=0;i<T;i++)
+    {
+        int n;
+        cout << "Enter the size of array: ";
+        cin >> n;
+        int arr[n];
+        cout << "Enter the array elements: ";
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        findSequence(arr, n);
+    }
+    return 0;
+}
